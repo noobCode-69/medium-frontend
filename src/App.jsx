8040fdeaaac2dad316de3682cbedac3642dd4870
noobCode-69 <armanyppph@gmail.com>
2023-02-25 20:03:08 +0530

    added cookie on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CookiesProvider } from "react-cookie";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -13,19 +14,21 @@ import EditProfile from "./components/EditProfile/EditProfile";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/feed/:feedId" element={<Feed />} />
-        <Route path="/article/:articleId" element={<Article />} />
-        <Route path="/profile/:profileId" element={<Profile />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-      </Routes>
-    </Router>
+    <CookiesProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/feed/:feedId" element={<Feed />} />
+          <Route path="/article/:articleId" element={<Article />} />
+          <Route path="/profile/:profileId" element={<Profile />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+        </Routes>
+      </Router>
+    </CookiesProvider>
   );
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import style from "./Home.module.css";
 import HomeNav from "./HomeNav/HomeNav";
 import medium from "../../assets/medium.png";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [cookie, setCookie] = useCookies(["visited"]);
+
+  useEffect(() => {
+    if (cookie.visited == undefined) {
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 1);  
+      console.log(expires); 
+      setCookie("visited", "true", { path: "/" , expires });
+    } else {
+      navigate("/feed/home");
+    }
+  }, []);
+
   return (
     <div>
       <HomeNav />
@@ -14,7 +30,12 @@ const Home = () => {
             <div className={style["h1-container"]}>
               <h1>Stay</h1>
               <div className={style["h1-levels"]}>
-                <h1 style={{marginTop : "-1.75rem"}} className={style["level1"]}>Curious.</h1>
+                <h1
+                  style={{ marginTop: "-1.75rem" }}
+                  className={style["level1"]}
+                >
+                  Curious.
+                </h1>
               </div>
             </div>
             <div className={style["buttons"]}>
@@ -27,7 +48,7 @@ const Home = () => {
             </div>
           </div>
           <img src={medium} />
-          <p className={style['details']}>
+          <p className={style["details"]}>
             author : &nbsp; <a>so_hell</a>
           </p>
         </div>
